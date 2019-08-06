80b4f35b2ca9ffb851b6d844f516092eadf86c00
nguvanha <52674146+nguvanha@users.noreply.github.com>
2019-08-06 17:03:22 -0400

    Add files via upload

---
+++ assets/data/nguvanhaInfo.ts
@@ -0,0 +1,8 @@
+import { nguvanha } from '../../app/nguvanha';
+
+export const NGUVANHAINFO: nguvanha = 
+    { snumber: 991497440, 
+        sname: 'Van Ha Nguyen', 
+        slogin: 'nguvanha', 
+        scampus: 'Davis Campus', 
+        assigntitle: 'Assignment 4'};
